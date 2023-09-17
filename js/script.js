d9f34d5d25451e1c8e53047c79f02a8aef8a9a9a
R-G2110 <argeldelacruz86@gmail.com>
2023-09-18 00:18:54 +0200

    [Fix]: verifica palindromo on script.js

--- js/script.js
+++ js/script.js
@@ -20,38 +20,25 @@ giocoPariDispari.addEventListener('click', function (){
 
 /*Palindromo*/
 //note: da mettere in lowercase  
-const parola = [];
-const clone = [];
-// let i = 0;
+
 let palindromo;
 const verify = document.getElementById('btn-verifica');
+const resetPalindromo = document.querySelector('#palindromo .btn-reset');
 const inputWord = document.getElementById('input-word');
 const outputPalindromo = document.getElementById('output-palindromo');
-const resetPalindromo = document.querySelector('#palindromo .btn-reset');
 
 verify.addEventListener('click', function(){
-	let i = 0;
 	const word = inputWord.value;
-	for(let i = 0; i < word.length; i++){
-		parola[i] = word.charAt(i);
-		console.log('parola: ' + parola);
-	};
-	for(let c = (word.length-1); c >= 0 ; c--){
-		clone[i] = word.charAt(c);
-		console.log('clone: ' + clone);
-		i++;
-	};
-	for (let i = 0; i < word.length; i++){
-		if (parola[i] === clone[i]) {
-			palindromo = 'è una parola palindroma';
-		} else {
-			palindromo = 'non è una parola palindroma';
-		}
-		
+	
+	if (verificaDati(word)){
+		palindromo = verificaParolaPalindroma(word);
+	} else {
+		palindromo = 'Attenzione!!! non hai inserito nessuna parola!!!';
 	}
+
 	outputPalindromo.classList.remove('d-none');
-	outputPalindromo.innerHTML = "'" + word +"'" + ' ' + palindromo;
-	console.log(palindromo);
+	outputPalindromo.innerHTML = word + ' ' + palindromo;
+	
 });
 
 //Resetta il dato inserito (parola) e nasconde il messaggio del risultato
@@ -59,70 +46,9 @@ resetPalindromo.addEventListener('click', function (){
 	inputWord.value = '';
 	outputPalindromo.classList.add('d-none');
 });
-
-// for(let i = 0; i < word.length; i++){
-// 	parola[i] = word.charAt(i);
-// 	console.log('parola: ' + parola);
-// };
-
-// for(let c = (word.length-1); c >= 0 ; c--){
-// 	clone[i] = word.charAt(c);
-// 	console.log('clone: ' + clone);
-// 	i++;
-// };
-
-// for (let i = 0; i < word.length; i++){
-// 	if (parola[i] === clone[i]) {
-// 		palindromo = true;
-// 	} else palindromo = false;
-	
-// }
-
-// console.log(palindromo);
-
-/************************************************************* */
-// const word = 'onna';
-// let palindromo = false;
-// let loop = true;
-// let contatore = 0;
-// while(loop){
-// 	let inizio = contatore;
-// 	let fine = word.length - 1 - contatore;
-
-// 	if(inizio === fine || inizio > fine){
-// 		loop = false;
-// 	}else{
-// 		if(word.charAt(inizio) === word.charAt(fine)){
-// 			palindromo = true;
-// 		}else{
-// 			palindromo = false;
-// 			loop = false;
-// 		}
-// 		contatore++;
-// 	}
-// }
-// const verify = document.getElementById('btn-verifica');
-// const inputWord = document.getElementById('input-word');
-// let palindromo = false;
-
-// verify.addEventListener('click', function(){
-// 	const word = inputWord.innerHTML;
-// 	for (let i = 0; i < word.length; i++){
-// 		if (inputWord.charAt(i) === word.charAt((word.length - 1) - i)){
-// 			palindromo = true;
-// 		} else {
-// 			palindromo = false;
-// 		}
-// 	}
-// });
-
-
-
-// console.log(palindromo);
-
-
-
-/*********************************************************************** */
+/*************************************************************************/
+/*************************************************************************/
+/*************************************************************************/
 /* Pari o Dispari*/
 const play = document.getElementById('btn-play');
 const inputNumber = document.getElementById('input-number');
@@ -137,12 +63,12 @@ play.addEventListener('click', function(){
 	const userOddEven = inputOddEven.value;
 	let message;
 
-	if(verificaDati(userNumber, userOddEven)){
+	if(verificaDati(userNumber) && verificaDati(userOddEven)){
 		const pcNumber = randomizer(1, 5);
 		const sum = somma(userNumber, pcNumber);
 		const sommaPariDispari = pariDispari(sum);
 
-		message = printMessage(vintoPariDispari(sommaPariDispari, userOddEven));
+		message = printMessageVinto(vintoPariDispari(sommaPariDispari, userOddEven));
 		output.classList.remove('d-none');
 		output.innerHTML = `Il computer ha giocato il numero ${pcNumber} e la somma è ${sum}.`;	
 	} else {
@@ -159,15 +85,39 @@ resetPariDispari.addEventListener('click', function (){
 	messageOutput.classList.add('d-none');
 	output.classList.add('d-none');
 });
-
+/************************************************************************/
+/************************************************************************/
 /**************************FUNCTIONS********************************/
 /**
- * Verifica se i dati inseriti = null,  restituisce t/f
- * @param {number inserted} number 
- * @param {option inserted} option 
+ * Verifica la stringa se è palindroma e resituisce uno dei messaggi:
+ * 1. è una parola palindroma
+ * 2. non è una parola palindroma
+ * @param {string to be verified} string 
+ * @returns 
  */
-function verificaDati(number, option){
-	if(number === "" || option === "" ) result = false;
+function verificaParolaPalindroma (string){
+	let condition = true;
+	let result;
+
+	for(let i = 0; i < string.length && condition; i++){
+		if (string.charAt(i) === string.charAt((string.length - 1) - i)){
+			condition = true;
+		} else  {
+			condition = false;
+		}
+	};
+	if (condition) result = 'è una parola palindroma!';
+	else result = 'non è una parola palindroma!';
+	
+	return result;
+}
+
+/**
+ * Verifica se il dato inserito = null,  restituisce t/f 
+ * @param {data inserted} data 
+ */
+function verificaDati(data){
+	if(data === "") result = false;
 	else result = true;
 	return result;
 };
@@ -179,7 +129,7 @@ function verificaDati(number, option){
  * @param {boolean(t/f) resultPariDispari} result 
  * @returns 
  */
-function printMessage(result){
+function printMessageVinto(result){
 	if (result){
 		message = ('Hai vinto!');
 	} else message = ('Hai perso!');
